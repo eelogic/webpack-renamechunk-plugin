12c05f8cdc947c78d7d1fb5a5838aefb6a215ed9
iwisunny <iwisunny@gmail.com>
2018-04-17 15:04:18 +0800

    upgrade: compat with webpack3+, optimize index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // plugin definition
-var RenameChunkPlugin = function(options) {};
+var RenameChunkPlugin = function(options) {
+  this.options = options;
+};
 
 // determine wheter a chunk is a splitted chunk
 // a splitted chunk is unnamed, not an entry and not initial
 RenameChunkPlugin.prototype.isSplitChunk = function(chunk) {
-  return !chunk.name && !chunk.entry && !chunk.initial;
+  try{
+    // only support in webpack v1
+    return !chunk.name && !chunk.entry && !chunk.initial;
+  }
+  catch (err){
+    // compat with webpack 3+
+    return !chunk.name && !chunk.hasEntryModule() && !chunk.canBeInitial();
+  }
 };
 
 // get a trunk infomation given a filename
@@ -19,23 +28,24 @@ RenameChunkPlugin.prototype.getChunkByFilename = function(chunks, filename) {
 // reanme chunks handler
 RenameChunkPlugin.prototype.renameChunks = function(compilation, opts) {
   // get split chunks
-  var splitChunks = compilation.chunks.filter(function(chunk) {
-    return this.isSplitChunk(chunk);
-  }.bind(this));
+  var splitChunks = compilation.chunks.filter(this.isSplitChunk);
 
   // rename assets files
   var assets = compilation.assets;
   var format = opts.renameChunkFileName;
   var filename, formatName, chunk;
   for (filename in assets) {
-    chunk = this.getChunkByFilename(splitChunks, filename);
-    if (chunk) {
-      formatName = format.replace(/\[id\]/g, String(chunk.id))
-        .replace(/\[name\]/g, chunk.name)
-        .replace(/\[hash\]/g, compilation.hash)
-        .replace(/\[chunkhash\]/g, chunk.hash);
-      assets[formatName] = assets[filename];
-      delete assets[filename];
+    if(assets.hasOwnProperty(filename)){
+      chunk = this.getChunkByFilename(splitChunks, filename);
+      if (chunk) {
+        formatName = format
+          .replace(/\[id\]/g, String(chunk.id))
+          .replace(/\[name\]/g, chunk.name)
+          .replace(/\[hash\]/g, compilation.hash)
+          .replace(/\[chunkhash\]/g, chunk.hash);
+        assets[formatName] = assets[filename];
+        delete assets[filename];
+      }
     }
   }
 };
@@ -45,7 +55,10 @@ RenameChunkPlugin.prototype.apply = function(compiler) {
   compiler.plugin('emit', function(compilation, callback) {
     var opts = compiler.options.output;
     var chunkFilename = opts && opts.chunkFilename;
-    var renameChunkFileName = opts && opts.renameChunkFileName;
+
+    // in webpack3+ output configuration not support custom prop like `renameChunkFileName`
+    // but we still compat with webpack v1
+    var renameChunkFileName = opts && opts.renameChunkFileName || this.options.renameChunkFileName;
 
     if (chunkFilename && renameChunkFileName) {
       this.renameChunks(compilation, {
