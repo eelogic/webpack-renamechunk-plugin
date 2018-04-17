f78c19dfebc513f9086b1fc08c87651d8a8a8db8
iwisunny <iwisunny@gmail.com>
2018-04-17 15:28:44 +0800

    update README, add prettier to format code

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ var RenameChunkPlugin = function(options) {
 // determine wheter a chunk is a splitted chunk
 // a splitted chunk is unnamed, not an entry and not initial
 RenameChunkPlugin.prototype.isSplitChunk = function(chunk) {
-  try{
+  try {
     // only support in webpack v1
     return !chunk.name && !chunk.entry && !chunk.initial;
-  }
-  catch (err){
+  } catch (err) {
     // compat with webpack 3+
     return !chunk.name && !chunk.hasEntryModule() && !chunk.canBeInitial();
   }
@@ -35,7 +34,7 @@ RenameChunkPlugin.prototype.renameChunks = function(compilation, opts) {
   var format = opts.renameChunkFileName;
   var filename, formatName, chunk;
   for (filename in assets) {
-    if(assets.hasOwnProperty(filename)){
+    if (assets.hasOwnProperty(filename)) {
       chunk = this.getChunkByFilename(splitChunks, filename);
       if (chunk) {
         formatName = format
@@ -52,23 +51,27 @@ RenameChunkPlugin.prototype.renameChunks = function(compilation, opts) {
 
 // plugin procedure
 RenameChunkPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('emit', function(compilation, callback) {
-    var opts = compiler.options.output;
-    var chunkFilename = opts && opts.chunkFilename;
+  compiler.plugin(
+    'emit',
+    function(compilation, callback) {
+      var opts = compiler.options.output;
+      var chunkFilename = opts && opts.chunkFilename;
 
-    // in webpack3+ output configuration not support custom prop like `renameChunkFileName`
-    // but we still compat with webpack v1
-    var renameChunkFileName = opts && opts.renameChunkFileName || this.options.renameChunkFileName;
+      // in webpack3+ output configuration not support custom prop like `renameChunkFileName`
+      // but we still compat with webpack v1
+      var renameChunkFileName =
+        (opts && opts.renameChunkFileName) || this.options.renameChunkFileName;
 
-    if (chunkFilename && renameChunkFileName) {
-      this.renameChunks(compilation, {
-        chunkFilename: chunkFilename,
-        renameChunkFileName: renameChunkFileName
-      });
-    }
+      if (chunkFilename && renameChunkFileName) {
+        this.renameChunks(compilation, {
+          chunkFilename: chunkFilename,
+          renameChunkFileName: renameChunkFileName
+        });
+      }
 
-    callback();
-  }.bind(this));
+      callback();
+    }.bind(this)
+  );
 };
 
 module.exports = RenameChunkPlugin;
